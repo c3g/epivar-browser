d997d9b93e91a0eddc8da7eec47b4c28fc822ab5
Rom Grk <romgrk.cc@gmail.com>
2020-10-29 16:19:02 -0400

    fix: finish removal of dbIHEC

--- models/tracks.js
+++ models/tracks.js
@@ -29,44 +29,8 @@ module.exports = {
 const range = { start: 0, end: 1000 }
 
 function get(chrom, position, assay = undefined) {
-
-  const makeQuery = samples => `
-      SELECT track.id
-           , institution.short_name
-           , sample_name
-           , donor
-           , track.view
-           , track.track_type
-           , track.id as id
-           , assembly.name as assembly
-           , assay.name as assay
-           , assay.id as assay_id
-           , assay_category.name as assay_category
-           , assay_category.id as assay_category_id
-        FROM dataset_track as track
-        JOIN dataset on dataset.id = track.dataset_id
-        JOIN assay on assay.id = dataset.assay_id
-        JOIN assay_category on assay_category.id = assay.assay_category_id
-        JOIN data_release on data_release.id = dataset.data_release_id
-        JOIN assembly on assembly.id = data_release.assembly_id
-        JOIN institution on institution.id = data_release.provider_institution_id
-       WHERE donor IN (${samples.map(dbIHEC.escape).join(', ')})
-              AND track_type = 'bigWig'
-              AND assembly.name = 'hg19'
-      ${ typeof assay === 'string' ?
-             `AND assay.name = ${dbIHEC.escape(assay)}` : ''
-      }
-    `
-
   return Samples.queryMap(chrom, position).then(info => {
-    return dbIHEC.query(makeQuery(Object.keys(info.samples)))
-    .then(tracks => {
-      tracks.forEach(track => {
-        track.path = getLocalPath(track)
-        Object.assign(track, info.samples[track.donor])
-      })
-      return tracks
-    })
+    return source.getTracks(info.samples, assay)
   })
 }
 
@@ -188,22 +152,6 @@ function mergeFiles(paths, { chrom, start, end }) {
       })
     )
     .then(() => ({ path: mergePath, url, hasDeviation: paths.length > 1 }))
-}
-
-function getLocalPath(track) {
-  return path.join(
-    config.paths.tracks,
-    track.short_name,
-    track.assembly,
-    [
-      track.id,
-      track.short_name,
-      track.sample_name,
-      track.assay,
-      track.view,
-      track.track_type
-    ].join('.')
-  )
 }
 
 function filterTracksUniqueDonorAssay(tracks) {
