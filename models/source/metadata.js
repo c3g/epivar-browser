db68d0707417bb58aad2e584e32c11ce13a4d00e
Rom Grk <romgrk.cc@gmail.com>
2020-11-17 10:50:59 -0500

    fix: source metadata: retrieve correct sample for track

--- models/source/metadata.js
+++ models/source/metadata.js
@@ -12,18 +12,33 @@ module.exports = {
   getTracks,
 }
 
-function getTracks(samples, assay) {
+const nameToRealName = name =>
+  name.split('_')[1]
 
-  const sampleNames = Object.keys(samples)
+/**
+ * @param {Object.<string, Object>} samples
+ * @param {string} assay
+ */
+function getTracks(samples, assay) {
+  /*
+   * The current gemini database contains names as "Epi_realName_flu_xxx".
+   * We need to extract "realName" to make it easier for the rest.
+   */
+  const samplesByRealName =
+    Object.fromEntries(
+      Object.entries(samples)
+        .map(([key, value]) => [nameToRealName(key), value]))
+
+  const sampleNames = Object.keys(samplesByRealName)
 
   const tracks =
     metadata
-      .filter(track => sampleNames.some(s => s.includes(`_${track.donor}_`)))
+      .filter(track => sampleNames.includes(track.donor))
       .map(clone)
 
   tracks.forEach(track => {
     track.path = getLocalPath(track)
-    Object.assign(track, samples[track.donor])
+    Object.assign(track, samplesByRealName[track.donor])
   })
 
   return Promise.resolve(tracks)
