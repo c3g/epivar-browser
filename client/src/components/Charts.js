1a0a1156de37fa4ff9127c7a9d1dc9e854b03fc0
Rom Grk <romgrk.cc@gmail.com>
2020-12-11 09:10:19 -0500

    client: fix grouping by ethnicity for tracks data

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -166,16 +166,21 @@ function makeSubgroups(list, n) {
 }
 
 function getDataFromValues(values) {
-  return Object.entries(values.map).map(([assay, valuesByType]) =>
-    ({
-      assay,
-      data: [
-        { name: 'Hom Ref', data: valuesByType.REF || [] },
-        { name: 'Het',     data: valuesByType.HET || [] },
-        { name: 'Hom Alt', data: valuesByType.HOM || [] }
-      ]
-    })
+  return Object.entries(values.map)
+  .map(([assay, valuesByEthnicity]) =>
+    Object.entries(valuesByEthnicity).map(([ethnicity, valuesByType]) =>
+      ({
+        assay,
+        ethnicity,
+        data: [
+          { name: 'Hom Ref', data: valuesByType.REF || [] },
+          { name: 'Het',     data: valuesByType.HET || [] },
+          { name: 'Hom Alt', data: valuesByType.HOM || [] }
+        ]
+      })
+    )
   )
+  .flat()
 }
 
 
