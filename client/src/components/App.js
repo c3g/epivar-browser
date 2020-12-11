e936b5b283701fc55b5d95369477556ddda7a20a
Rom Grk <romgrk.cc@gmail.com>
2020-12-11 09:42:01 -0500

    refactor(client): extract ParamsTable

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,64 +1,25 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
-import { Table } from 'reactstrap';
 
-import Header from './Header.js'
-import Controls from './Controls.js'
-import Charts from './Charts.js'
+import Charts from './Charts'
+import Controls from './Controls'
+import Header from './Header'
+import ParamsTable from './ParamsTable'
 
 
-const mapStateToProps = state => ({
-    isLoading: state.samples.isLoading
-  , samples: state.samples.stats
-  , values: state.values
-})
+const mapStateToProps = state => ({})
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ }, dispatch)
+  bindActionCreators({}, dispatch)
 
 class App extends Component {
 
   render() {
-    const { isLoading, samples } = this.props
-    const counts = Object.entries(samples.counts).map(([key, value]) => ({ key: key, count: value }))
-
-    const showParams = !isLoading && samples.total > 0
-
     return (
       <div className='App'>
-
         <Header>
           <Controls />
-
-          <Table className={ 'Params ' + ( showParams ? 'visible' : '' ) }>
-            <thead>
-              <tr>
-                <th>Chrom</th>
-                <th>Start</th>
-                <th>End</th>
-                <th>Reference</th>
-                {
-                  counts.map(({key}) =>
-                    <th key={key}>{key}</th>
-                  )
-                }
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{ samples.chrom }</td>
-                <td>{ samples.start }</td>
-                <td>{ samples.end }</td>
-                <td>{ samples.ref }</td>
-                {
-                  counts.map(({key, count}) =>
-                    <th key={key}>{count}</th>
-                  )
-                }
-              </tr>
-            </tbody>
-          </Table>
-
+          <ParamsTable />
         </Header>
 
         <Charts />
