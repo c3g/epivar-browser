c95e837c6d7297da5a806f4e877d755270ed33f8
David Lougheed <david.lougheed@gmail.com>
2021-09-27 12:56:51 -0400

    feat: click title to go home

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container } from 'reactstrap'
+import {useHistory} from "react-router-dom";
 
 export default function Header({ children }) {
+  const history = useHistory();
 
   return (
     <div className='Header'>
       <Container>
-        <h1 className='Header__title'>IMMUNPOP</h1>
+        <h1 className='Header__title' onClick={() => history.push("/")}>IMMUNPOP</h1>
         <h4 className='Header__subtitle'>Epigenetic & Expression QTLs</h4>
         { children }
       </Container>
