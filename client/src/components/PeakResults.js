b42926e958444932bf076ca1baf0ea70602e62b3
David Lougheed <david.lougheed@gmail.com>
2021-09-27 13:17:48 -0400

    convert peakresults to functional component

--- client/src/components/PeakResults.js
+++ client/src/components/PeakResults.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {useState} from 'react'
+import {useSelector} from 'react-redux';
 import { Container, TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap'
 import { groupBy, sortBy, prop, map, compose } from 'rambda'
 import memoizeOne from 'memoize-one'
@@ -8,98 +8,71 @@ import cx from 'clsx'
 import Icon from './Icon'
 import PeakAssay from './PeakAssay'
 
-const mapStateToProps = state => ({
-  isLoading: state.peaks.isLoading,
-  isLoaded: state.peaks.isLoaded,
-  isEmpty: state.peaks.isLoaded && state.peaks.list.length === 0,
-  peaks: state.peaks.list || [],
-})
-const mapDispatchToProps = {}
-
 // Original data comes sorted by order of priority, therefore we
 // sort by ID because it's also the priority field.
 const groupAndSortPeaks = memoizeOne(
   compose(map(sortBy(prop('valueNI'))), groupBy(prop('assay')))
 )
 
-class PeakResults extends Component {
-  state = {
-    activeTab: null,
+const PeakResults = () => {
+  const [activeTab, setActiveTab] = useState(null);
+  const isLoading = useSelector(state => state.peaks.isLoading);
+  const isLoaded = useSelector(state => state.peaks.isLoaded);
+  const isEmpty = useSelector(state => state.peaks.isLoaded && state.peaks.list.length === 0);
+  const peaks = useSelector(state => state.peaks.list || []);
+
+  const peaksByAssay = groupAndSortPeaks(peaks);
+  const assays = Object.keys(peaksByAssay);
+  const entries = Object.entries(peaksByAssay);
+
+  if (activeTab !== null && !(activeTab in peaksByAssay)) {
+    // Assay isn't valid for the position in question
+    setActiveTab(assays.length ? assays[0] : null);
+  } else if (activeTab === null && assays.length) {
+    setActiveTab(assays[0]);
   }
 
-  setTab = activeTab => {
-    this.setState({ activeTab })
-  }
+  return <div className={'PeakResults ' + (isLoading ? 'loading' : '')}>
+    {
+      isEmpty &&
+      <Container>
+        <div className='PeakResults__empty'>
+          No results for the selected range.<br/>
+          Try with a different range.
+        </div>
+      </Container>
+    }
+    {
+      (isLoading || isLoaded) &&
+      <Container>
+        <Nav tabs>
+          {
+            entries.map(([assay, peaks]) =>
+              <NavItem key={assay}>
+                <NavLink
+                  className={cx({ active: activeTab === assay })}
+                  onClick={() => setActiveTab(assay)}
+                >
+                  <Icon name='flask' className='PeakAssay__icon' />
+                  <strong>{assay.replace("Chipmentation ", "")}</strong>&nbsp;-&nbsp;
+                  {peaks.length} peak{peaks.length > 1 ? 's' : ''}
+                </NavLink>
+              </NavItem>
+            )
+          }
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          {
+            entries.map(([assay, peaks]) =>
+              <TabPane key={assay} tabId={assay}>
+                <PeakAssay assay={assay} peaks={peaks} />
+              </TabPane>
+            )
+          }
+        </TabContent>
+      </Container>
+    }
+  </div>;
+};
 
-  static getDerivedStateFromProps(props, state) {
-    const peaksByAssay = groupAndSortPeaks(props.peaks || [])
-    const assays = Object.keys(peaksByAssay)
-
-    if (state.activeTab !== null && !(state.activeTab in peaksByAssay))
-      return { activeTab: assays.length > 0 ? assays[0] : null }
-
-    if (state.activeTab === null && assays.length > 0)
-      return { activeTab: assays[0] }
-
-    return state
-  }
-
-  render() {
-    const { activeTab } = this.state
-    const { isLoading, isLoaded, isEmpty, peaks } = this.props
-
-    const peaksByAssay = groupAndSortPeaks(peaks)
-    const entries = Object.entries(peaksByAssay)
-
-    // Even though in the scripts we *add* chipmentation to the assay name for consistency, we remove it here
-    // for display purposes.
-    return (
-      <div className={'PeakResults ' + (isLoading ? 'loading' : '')}>
-        {
-          isEmpty &&
-            <Container>
-              <div className='PeakResults__empty'>
-                No results for the selected range.<br/>
-                Try with a different range.
-              </div>
-            </Container>
-        }
-        {
-          (isLoading || isLoaded) &&
-            <Container>
-              <Nav tabs>
-                {
-                  entries.map(([assay, peaks]) =>
-                    <NavItem key={assay}>
-                      <NavLink
-                        className={cx({ active: activeTab === assay })}
-                        onClick={() => this.setTab(assay)}
-                      >
-                        <Icon name='flask' className='PeakAssay__icon' />
-                        <strong>{assay.replace("Chipmentation ", "")}</strong>&nbsp;-&nbsp;
-                        {peaks.length} peak{peaks.length > 1 ? 's' : ''}
-                      </NavLink>
-                    </NavItem>
-                  )
-                }
-              </Nav>
-              <TabContent activeTab={activeTab}>
-                {
-                  entries.map(([assay, peaks]) =>
-                    <TabPane key={assay} tabId={assay}>
-                      <PeakAssay assay={assay} peaks={peaks} />
-                    </TabPane>
-                  )
-                }
-              </TabContent>
-            </Container>
-        }
-      </div>
-    )
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PeakResults);
+export default PeakResults;
