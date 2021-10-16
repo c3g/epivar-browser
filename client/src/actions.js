137ae4f0c494a0360b42b3a63dd20e9fe0b24956
David Lougheed <david.lougheed@gmail.com>
2021-10-16 15:00:46 -0400

    revert reversion!

--- client/src/actions.js
+++ client/src/actions.js
@@ -51,7 +51,7 @@ export function mergeTracks(peak) {
         db,
         hubClear: hubURL,
         position,
-        highlight: `${db}.${position}#FFAAAA`,
+        highlight: `${db}.${session.chrom}:${session.position}-${session.position+1}#FFAAAA`,
       })
 
       console.log('Hub:',  hubURL)
