58bbfdc188ec77a1379bea785c5d6ade30308a91
David Lougheed <david.lougheed@gmail.com>
2022-01-31 16:08:09 -0500

    fix: box plot titles

--- client/src/helpers/conditions.js
+++ client/src/helpers/conditions.js
@@ -2,6 +2,6 @@ export const CONDITION_NI = "NI";
 export const CONDITION_FLU = "Flu";
 
 export const conditionName = (c) => ({
-  CONDITION_NI: "Non-infected",
-  CONDITION_FLU: "Flu",
+  [CONDITION_NI]: "Non-infected",
+  [CONDITION_FLU]: "Flu",
 }[c] ?? "Unknown");
