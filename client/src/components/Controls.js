7794e46e3c9a4238d7ead220bd743b207ef00d74
David Lougheed <david.lougheed@gmail.com>
2022-05-02 20:30:27 -0400

    fix: misc issues with controls chore: more relevant help text depending on selected 'chrom' refact: rewrite controls as functional component

--- client/src/components/Controls.js
+++ client/src/components/Controls.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, {useCallback, useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import axios from 'axios'
 import debounce from 'lodash/debounce'
 import {
@@ -18,88 +18,160 @@ import {
   setChrom,
   doSearch,
   setPosition,
-  fetchSamples,
   fetchPositions,
-  handleError,
 } from '../actions.js'
-
-const mapStateToProps = state => ({
-  isLoading: state.samples.isLoading,
-  samples: state.samples.list,
-  chroms: state.chroms,
-  chrom: state.ui.chrom,
-  windowStart: state.ui.windowStart,
-  windowEnd: state.ui.windowEnd,
-  positions: state.positions,
-  position: state.ui.position,
-  range: state.ui.range,
-  userData: state.user,
-})
-const mapDispatchToProps = {
-  setChrom,
-  doSearch,
-  setPosition,
-  fetchSamples,
-  fetchPositions,
-  handleError
-};
+import {useNavigate, useParams} from "react-router-dom";
 
 const defaultChrom = "rsID";
 
-class Controls extends React.Component {
+const renderChromName = chr => {
+  return ["gene", "rsID"].includes(chr) ? chr : `chr${chr}`;
+};
 
-  state = {
-    index: 0,
-    open: false,
-    didFirstSearch: false,
-    source: undefined,
-  }
+const ChromDropdown = ({chrom, chroms: {isLoading, list}, setChrom}) => {
+  return <UncontrolledDropdown className='Controls__chromosome input-group-prepend'>
+    <DropdownToggle caret disabled={isLoading}>
+      {isLoading && <Icon name='spinner' spin />}
+      {!isLoading && (chrom ? renderChromName(chrom) : 'Chrom.')}
+    </DropdownToggle>
+    <DropdownMenu>
+      {list.map(chr =>
+        <DropdownItem key={chr} onClick={() => setChrom(chr)}>
+          {renderChromName(chr)}</DropdownItem>
+      )}
+    </DropdownMenu>
+  </UncontrolledDropdown>;
+};
 
-  componentDidMount() {
-    const { params: {chrom, position} } = this.props;
-    if (chrom && position) {
-      this.props.setChrom(chrom);
-      this.changePosition(position);
-      this.props.doSearch();
+const Controls = ({toggleHelp}) => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const chroms = useSelector(state => state.chroms);
+  const chrom = useSelector(state => state.ui.chrom);
+  const positions = useSelector(state => state.positions);
+  const position = useSelector(state => state.ui.position);
+  const userData = useSelector(state => state.user);
+
+  const dispatch = useDispatch();
+
+  const {chrom: paramsChrom, position: paramsPosition} = params;
+  const {isLoading, list} = positions;
+
+  const [didFirstSearch, setDidFirstSearch] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [source, setSource] = useState(null);
+
+  const searchIsLongEnough = useCallback(() => {
+    const pStr = position.toString().toLowerCase();
+    return chrom !== "rsID"
+      || (pStr.startsWith("rs") && pStr.length > 2)
+      || !(pStr.startsWith("rs") && pStr.length);
+  }, [chrom, position]);
+
+  const debouncedFetch = useCallback(debounce(start => {
+    if (searchIsLongEnough()) {
+      if (source) {
+        console.log("canceling stale autocomplete request")
+        source.cancel("stale autocomplete request")
+      }
+      const newSource = axios.CancelToken.source();
+      setSource(newSource);
+      dispatch(fetchPositions({chrom, start}, undefined, newSource.token));
     }
-  }
+  }, 200, {leading: false, trailing: true}), [dispatch, searchIsLongEnough, chrom]);
 
-  componentWillUnmount() {
-    if (this.state.source) {
-      this.state.source.cancel()
+  const changePosition = useCallback(pos => dispatch(setPosition(pos)),
+    [dispatch]);
+
+  // --- Effects --------------------------------------------------------------
+
+  // On mount, if chrom & position are set as params, then populate the Redux state & immediately run a search
+  useEffect(() => {
+    if (paramsChrom && paramsPosition) {
+      dispatch(setChrom(paramsChrom));
+      changePosition(paramsPosition);
+      dispatch(doSearch());
     }
-  }
+  }, [dispatch]);
 
-  componentWillReceiveProps(nextProps, _nextContext) {
-    if (nextProps.params?.chrom !== this.props.params?.chrom) {
-      const { params: {chrom} } = nextProps;
-      this.props.setChrom(chrom ?? defaultChrom);
+  // If an autocomplete request is in motion when the component is unmounted, try to cancel it.
+  useEffect(() => () => {if (source) source.cancel();}, [source]);
+
+  // If the chromosome/search domain in the URL is changed, update the Redux state
+  useEffect(() => {
+    if (paramsChrom && !chrom) {
+      dispatch(setChrom(paramsChrom ?? defaultChrom));
     }
-    if (nextProps.chrom !== this.props.chrom) {
-      const {chrom} = nextProps
-      this.props.fetchPositions({ chrom, position: '' })
+  }, [dispatch, paramsChrom, chrom]);
+
+  // If the chromosome/search domain is changed in Redux, reset the position list
+  useEffect(() => {dispatch(fetchPositions({chrom, position: ""}));},
+    [dispatch, chrom]);
+
+  // If the position/search term is changed in Redux, fetch the latest autocomplete results
+  useEffect(() => {debouncedFetch(position);}, [position]);
+
+  // If the position data changes, reset the selected index
+  useEffect(() => {
+    setIndex(0);
+  }, [positions]);
+
+  // --------------------------------------------------------------------------
+
+  const onChange = useCallback(ev => changePosition(ev.target.value),
+    [changePosition]);
+
+  const onFocus = useCallback(() => {
+    setTimeout(() => setOpen(true), 100);
+  }, []);
+
+  const onBlur = useCallback(() => {
+    setTimeout(() => setOpen(false), 200);
+  }, []);
+
+  const selectItem = useCallback(index => {
+    /** @type {{
+     * nat_id: string,
+     * position: string,
+     * name: string,
+     * assay: string,
+     * minValueMin: number,
+     * nFeatures: number
+     * }} */
+    const item = list[index];
+
+    const position = item.nat_id ?? item.position ?? item.name;
+    // The item assay is the tab with the most significant result - which will be
+    // selected first by nature of ordering, thus leading the user to the most interesting
+    // detail from the autocomplete.
+    console.log(item);
+    navigate(`/explore/locus/${chrom}/${position}/${item.assay}`, {replace: true});
+    changePosition(position);
+    dispatch(doSearch());
+  }, [list, dispatch, navigate, changePosition]);
+
+  const moveSelection = useCallback(n => {
+    const {length} = list;
+    let newIndex = index + n;
+
+    if (index < 0) {
+      newIndex = newIndex + length;
+    } else if (index > length - 1) {
+      newIndex = newIndex % length;
     }
-    if (nextProps.positions !== this.props.positions) {
-      this.setState({ index: 0 })
-    }
-  }
 
-  onFocus = () => {
-    setTimeout(() => this.setState({ open: true }), 100)
-  }
+    setIndex(newIndex);
+  }, [list]);
 
-  onBlur = () => {
-    setTimeout(() => {
-      this.setState({ open: false })
-    }, 200)
-  }
-
-  onKeyDown = ev => {
+  const onKeyDown = useCallback(ev => {
     if (ev.which === 13 /* Enter */) {
-      if (this.props.positions.list.length > 0) {
-        this.selectItem(this.state.index);
+      if (list.length > 0) {
+        selectItem(index);
       } else {
-        this.props.doSearch();
+        dispatch(doSearch());
+        setDidFirstSearch(true);
       }
 
       if (document.activeElement.tagName === 'INPUT')
@@ -108,200 +180,106 @@ class Controls extends React.Component {
 
     if (ev.which === 9 /* Tab */) {
       ev.preventDefault()
-      if (ev.shiftKey)
-        this.moveSelection(-1)
-      else
-        this.moveSelection(+1)
+      // Shift-tab moves up, tab moves down
+      moveSelection(ev.shiftKey ? -1 : +1);
     }
     if (ev.which === 38 /* ArrowUp */) {
-      this.moveSelection(-1)
+      moveSelection(-1)
     }
     if (ev.which === 40 /* ArrowDown */) {
-      this.moveSelection(+1)
+      moveSelection(+1)
     }
-  }
+  }, [dispatch, selectItem, moveSelection]);
 
-  onClickSearch = () => {
-    this.props.doSearch();
-    this.setState({ didFirstSearch: true })
-  }
+  const setChromCb = useCallback(v => {
+    dispatch(setChrom(v));
+  }, [dispatch]);
 
-  searchIsLongEnough = () => {
-    const {chrom, position} = this.props;
-    const pStr = position.toString().toLowerCase();
-    return chrom !== "rsID"
-      || (pStr.startsWith("rs") && pStr.length > 2)
-      || !(pStr.startsWith("rs") && pStr.length);
-  }
+  const onClickSearch = useCallback(() => {
+    console.log(chrom, position);
+    navigate(`/explore/locus/${chrom}/${position}`, {replace: true});
+    dispatch(doSearch());
+  }, [navigate, chrom, position]);
 
-  debouncedFetch = debounce(start => {
-    const {chrom} = this.props;
-    if (this.searchIsLongEnough()) {
-      if (this.state.source) {
-        console.log("canceling stale autocomplete request")
-        this.state.source.cancel("stale autocomplete request")
-      }
-      this.setState({source: axios.CancelToken.source()}, () => {
-        this.props.fetchPositions({chrom, start}, undefined, this.state.source.token)
-      })
-    }
-  }, 200, {leading: false, trailing: true})
-
-  changePosition = pos => {
-    this.props.setPosition(pos);
-    this.debouncedFetch(pos);
-  }
-
-  onChange = (ev) => {
-    this.changePosition(ev.target.value)
-  }
-
-  moveSelection = n => {
-    const { length } = this.props.positions.list
-    let index = this.state.index + n
-
-    if (index < 0)
-      index = index + length
-    else if (index > length - 1)
-      index = index % length
-
-    this.setState({ index })
-  }
-
-  selectItem = index => {
-    const {positions: {list}, navigate, chrom} = this.props;
-    const item = list[index];
-    const position = item.nat_id ?? item.position ?? item.name;
-    // The item assay is the tab with the most significant result - which will be
-    // selected first by nature of ordering, thus leading the user to the most interesting
-    // detail from the autocomplete.
-    navigate(`/explore/locus/${chrom}/${position}/${item.assay}`, {replace: true});
-    this.changePosition(position);
-    this.props.doSearch();
-  }
-
-  renderChromName(chr) {
-    return ["gene", "rsID"].includes(chr) ? chr : `chr${chr}`;
-  }
-
-  renderChroms() {
-    const { chrom, chroms: { isLoading, list }, setChrom } = this.props
-
-    return (
-      <UncontrolledDropdown className='Controls__chromosome input-group-prepend'>
-        <DropdownToggle caret disabled={isLoading}>
-          { isLoading &&
-            <span><Icon name='spinner' spin/> Loading</span>
-          }
+  return <div className={cx('Controls', { didFirstSearch })}>
+    <div className='Controls__content'>
+      <div style={{display: "flex", flexDirection: "row", width: "100%", maxWidth: "550px"}}>
+        <InputGroup style={{flex: 1}}>
+          <ChromDropdown chrom={chrom} chroms={chroms} setChrom={setChromCb} />
+          <Input
+            className='Controls__input autocomplete'
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            value={position || ""}
+            disabled={!userData.isLoaded || !userData.data?.consentedToTerms}
+            placeholder={(chrom => {
+              switch (chrom) {
+                case 'rsID':
+                  return 'Search by RS ID';
+                case 'gene':
+                  return 'Search RNA-seq results by gene name';
+                default:
+                  return 'Search position';
+              }
+            })(chrom)}
+          />
           {
-            !isLoading &&
-              (chrom ? this.renderChromName(chrom) : 'Chrom.')
-          }
-        </DropdownToggle>
-        <DropdownMenu>
-          {
-            list.map(chr =>
-              <DropdownItem key={chr} onClick={() => setChrom(chr)}>
-                {this.renderChromName(chr)}</DropdownItem>
-            )
-          }
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    )
-  }
-
-  renderPosition() {
-    const { open, index } = this.state
-    const { chrom, position, positions: { isLoading, list }, userData } = this.props
-
-    return <>
-      <Input
-        className='Controls__input autocomplete'
-        onChange={this.onChange}
-        onKeyDown={this.onKeyDown}
-        onFocus={this.onFocus}
-        onBlur={this.onBlur}
-        value={position || ""}
-        disabled={!userData.isLoaded || !userData.data?.consentedToTerms}
-        placeholder={(chrom => {
-          switch (chrom) {
-            case 'rsID':
-              return 'Search by RS ID'
-            case 'gene':
-              return 'Search RNA-seq results by gene name'
-            default:
-              return 'Search position'
-          }
-        })(chrom)}
-      />
-      {
-        open &&
-          <div className='autocomplete__dropdown-menu'>
-            {
-              list.length === 0 && <div className={ 'autocomplete__item autocomplete__item--empty' }>
+            open &&
+            <div className='autocomplete__dropdown-menu'>
+              {list.length === 0 && <div className={ 'autocomplete__item autocomplete__item--empty' }>
                 <span>{
-                  this.searchIsLongEnough()
+                  searchIsLongEnough()
                     ? (isLoading ? "Loading..." : "No results")
                     : "Keep typing to see results"
                 }</span>
-              </div>
-            }
-            <div className={(list.length > 0 && isLoading) ? "loading" : ""}>
-              {
-                list.map((item, i) =>
+              </div>}
+              <div className={(list.length > 0 && isLoading) ? "loading" : ""}>
+                {list.map((item, i) =>
                   <div
                     key={item.nat_id ?? item.position ?? item.name}
                     className={ 'autocomplete__item ' + (i === index ? 'autocomplete__item--selected' : '') }
-                    onClick={() => this.selectItem(i)}
+                    onClick={() => selectItem(i)}
                   >
                     { highlight(item.nat_id ?? item.position ?? item.name, position) }
                     <span>
-                      <strong>Min. <span style={{fontFamily: "serif"}}>p</span> value:</strong>
+                          <strong>Min. <span style={{fontFamily: "serif"}}>p</span> value:</strong>
                       {' '}
                       {item.minValueMin.toExponential(2)}
-                    </span>
+                        </span>
                     <span><strong>Features:</strong> {item.nFeatures}</span>
                   </div>
-                )
-              }
-            </div>
-          </div>
-      }
-    </>;
-  }
-
-  render() {
-    const { didFirstSearch } = this.state
-    const { isLoading, userData } = this.props
-
-    return (
-      <div className={cx('Controls', { didFirstSearch })}>
-        <div className='Controls__content'>
-          <div style={{display: "flex", flexDirection: "row", width: "100%", maxWidth: "550px"}}>
-            <InputGroup style={{flex: 1}}>
-              { this.renderChroms() }
-              { this.renderPosition() }
-              <div className='input-group-append'>
-                <Button className='Controls__search'
-                  onClick={this.onClickSearch}
-                  disabled={isLoading || !userData.isLoaded || !userData.data?.consentedToTerms}
-                >
-                  <Icon name={ isLoading ? 'spinner' : 'search' } spin={isLoading} /> Search
-                </Button>
+                )}
               </div>
-            </InputGroup>
-            <InputGroup style={{marginLeft: "8px"}}>
-              <Button onClick={this.props.toggleHelp || (() => {})}>
-                <Icon name="question-circle-o" /></Button>
-            </InputGroup>
+            </div>
+          }
+          <div className='input-group-append'>
+            <Button className='Controls__search'
+                    onClick={onClickSearch}
+                    disabled={isLoading || !userData.isLoaded || !userData.data?.consentedToTerms}>
+              <Icon name={ isLoading ? 'spinner' : 'search' } spin={isLoading} /> Search
+            </Button>
           </div>
-          <div className='Controls__example'>
-            e.g.: <b>chr11</b> <b>70310556</b> <em>Select chromosome first, then position</em>
-          </div>
-        </div>
+        </InputGroup>
+        <InputGroup style={{marginLeft: "8px"}}>
+          <Button onClick={toggleHelp || (() => {})}>
+            <Icon name="question-circle-o" /></Button>
+        </InputGroup>
       </div>
-    )
-  }
+      <div className='Controls__example'>
+        {(!chrom || !["rsID", "gene"].includes(chrom)) && <>
+          e.g.: <b>chr11</b> <b>70310556</b> <em>Select chromosome first, then position</em>
+        </>}
+        {chrom === "rsID" && <>
+          e.g.: <b>rs35282812</b> <em>Start typing to search by rsID</em>
+        </>}
+        {chrom === "gene" && <>
+          e.g.: <b>DGKE</b> <em>Start typing to search by gene name (case insensitive)</em>
+        </>}
+      </div>
+    </div>
+  </div>;
 }
 
 /**
@@ -326,7 +304,4 @@ function highlight(item, prefix) {
   </span>;
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Controls);
+export default Controls;
