c46361bbdf7a2a7bc32e44295674aaa55d8e2821
David Lougheed <david.lougheed@gmail.com>
2022-05-05 17:09:10 -0400

    chore(web): add new FAQ entry

--- client/src/components/pages/FAQPage.js
+++ client/src/components/pages/FAQPage.js
@@ -18,7 +18,7 @@ const FAQPage = () => {
           </p>
         </details>
         <details open={true}>
-          <summary>How are the genomic tracks generated?</summary>
+          <summary>How were the genomic tracks grouped by genotype generated?</summary>
           <p>
             Genome browser tracks were created with the HOMER <em>makeUCSCfile</em> command and{" "}
             <em>bedGraphToBigWig</em> utility from UCSC. Tracks were normalized so that each value represents the read
@@ -28,6 +28,16 @@ const FAQPage = () => {
                rel="noreferrer">UCSC Genome Browser</a> track hubs upon
             request to visualize averaged track segments for a given genomic feature. These tracks are created on the
             fly by averaging bigWig regions of samples sharing an experimental treatment and genotype.
+          </p>
+        </details>
+        <details open={true}>
+          <summary>How were the global tracks per ancestry group generated?</summary>
+          <p>
+            The dark shaded area denotes the distribution of the average RPM values and the light shaded area denotes 
+            the standard deviation. Signals of various epigenetic marks are shown in blue color for non-infected samples 
+            and red color for infected samples. For RNA-seq, forward and reverse transcripts are shown in blue and green 
+            color separately for non-infected samples; while forward and reverse transcripts are shown in red and brown 
+            color separately for infected samples.
           </p>
         </details>
       </Col>
