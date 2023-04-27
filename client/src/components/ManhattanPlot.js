cf60d0973bfd7e19c301d4eff946e8f51d6174ee
David Lougheed <david.lougheed@gmail.com>
2023-04-27 15:21:04 -0400

    manhattan point click handle

--- client/src/components/ManhattanPlot.js
+++ client/src/components/ManhattanPlot.js
@@ -16,181 +16,182 @@ const Cytoband = React.memo(({start, end, containerWidth}) => {
   // TODO
 });
 
-const ManhattanPlot = React.memo(({data, positionProp, pValueProp, snpProp, featureProp, geneProp}) => {
-  const pxr = useDevicePixelRatio({maxDpr: 50});
-  const qt = useRef(null);
+const ManhattanPlot = React.memo(
+  ({data, positionProp, pValueProp, snpProp, featureProp, geneProp, onPointClick}) => {
+    const pxr = useDevicePixelRatio({maxDpr: 50});
+    const qt = useRef(null);
 
-  const halfPointSize = useMemo(() => POINT_SIZE * pxr * 0.5, [pxr]);
+    const halfPointSize = useMemo(() => POINT_SIZE * pxr * 0.5, [pxr]);
 
-  const dataNoNulls = useMemo(() => data.filter(d => !!d[pValueProp]), [data]);
+    const dataNoNulls = useMemo(() => data.filter(d => !!d[pValueProp]), [data]);
 
-  const x = useMemo(() => dataNoNulls.map(d => d[positionProp] / 1000000), [dataNoNulls]);
-  const y = useMemo(() => dataNoNulls.map(d => -1 * Math.log10(d[pValueProp])), [dataNoNulls]);
-  const finalData = useMemo(() => [[[], []], [x, y]], [x, y]);
+    const x = useMemo(() => dataNoNulls.map(d => d[positionProp] / 1000000), [dataNoNulls]);
+    const y = useMemo(() => dataNoNulls.map(d => -1 * Math.log10(d[pValueProp])), [dataNoNulls]);
+    const finalData = useMemo(() => [[[], []], [x, y]], [x, y]);
 
-  const maxY = useMemo(() => Math.max(...y) * 1.1, [y]);
+    const maxY = useMemo(() => Math.max(...y) * 1.1, [y]);
 
-  const drawPoints = useCallback((u, seriesIdx) => {
-    // The below function is adapted from uPlot example, used under the terms of the MIT license.
-    // See https://github.com/leeoniya/uPlot/blob/master/demos/scatter.html
-    /*
-    The MIT License (MIT)
+    const drawPoints = useCallback((u, seriesIdx) => {
+      // The below function is adapted from uPlot example, used under the terms of the MIT license.
+      // See https://github.com/leeoniya/uPlot/blob/master/demos/scatter.html
+      /*
+      The MIT License (MIT)
 
-    Copyright (c) 2022 Leon Sorokin
+      Copyright (c) 2022 Leon Sorokin
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
+      The above copyright notice and this permission notice shall be included in
+      all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-     */
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+      THE SOFTWARE.
+       */
 
-    const newQt = quadtree();
+      const newQt = quadtree();
 
-    uPlot.orient(u, seriesIdx, (
-      series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim, moveTo, lineTo, rect, arc
-    ) => {
-      const d = u.data[seriesIdx];
+      uPlot.orient(u, seriesIdx, (
+        series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim, moveTo, lineTo, rect, arc
+      ) => {
+        const d = u.data[seriesIdx];
 
-      u.ctx.save();
+        u.ctx.save();
 
-      u.ctx.rect(u.bbox.left, u.bbox.top, u.bbox.width, u.bbox.height);
-      u.ctx.clip();
+        u.ctx.rect(u.bbox.left, u.bbox.top, u.bbox.width, u.bbox.height);
+        u.ctx.clip();
 
-      u.ctx.fillStyle = series.fill();
-      u.ctx.strokeStyle = series.stroke();
-      u.ctx.lineWidth = STROKE_WIDTH;
+        u.ctx.fillStyle = series.fill();
+        u.ctx.strokeStyle = series.stroke();
+        u.ctx.lineWidth = STROKE_WIDTH;
 
-      const p = new Path2D();
+        const p = new Path2D();
 
-      for (let i = 0; i < d[0].length; i++) {
-        const x = d[0][i];
-        const y = d[1][i];
+        for (let i = 0; i < d[0].length; i++) {
+          const x = d[0][i];
+          const y = d[1][i];
 
-        if (x >= scaleX.min && x <= scaleX.max && y >= scaleY.min && y <= scaleY.max) {
-          const cx = valToPosX(x, scaleX, xDim, xOff);
-          const cy = valToPosY(y, scaleY, yDim, yOff);
-          p.moveTo(cx + halfPointSize + STROKE_WIDTH / 2, cy);
-          arc(p, cx + STROKE_WIDTH / 2, cy + STROKE_WIDTH / 2, halfPointSize, 0, TAU);
+          if (x >= scaleX.min && x <= scaleX.max && y >= scaleY.min && y <= scaleY.max) {
+            const cx = valToPosX(x, scaleX, xDim, xOff);
+            const cy = valToPosY(y, scaleY, yDim, yOff);
+            p.moveTo(cx + halfPointSize + STROKE_WIDTH / 2, cy);
+            arc(p, cx + STROKE_WIDTH / 2, cy + STROKE_WIDTH / 2, halfPointSize, 0, TAU);
 
-          // D3-quadtree: index 0 is X, index 1 is Y, rest can be other stuff
-          newQt.add([
-            // cx - halfPointSize - STROKE_WIDTH / 2 - u.bbox.left,
-            cx - u.bbox.left - halfPointSize - STROKE_WIDTH,
-            // cy - halfPointSize - STROKE_WIDTH / 2 - u.bbox.top,
-            cy - u.bbox.top - halfPointSize - STROKE_WIDTH,
-            i,
-          ]);
-        }
-      }
-
-      u.ctx.fill(p);
-      u.ctx.stroke(p);
-      u.ctx.restore();
-    });
-
-    qt.current = newQt;
-    return null;
-  }, [pxr]);
-
-  const hoveredItem = useRef(undefined);
-
-  // noinspection JSUnusedGlobalSymbols
-  const uPlotOptions = useMemo(() => ({
-    title: "chr1 RNA-seq: Most significant peaks by SNP position (25kb bins)",
-    mode: 2, // ?
-    width: 1110,
-    height: 300,
-    scales: {
-      x: {time: false},
-      y: {range: [1, maxY]},
-    },
-    axes: [
-      {
-        label: "Position",
-        // scale: "Mb",
-        values: (self, ticks) => ticks.map(v => `${v.toFixed(0)} Mb`),
-      },
-      {label: "-log10(p)"},
-    ],
-    series: [
-      {},  // weird uPlot hack to make scatter plots
-      {
-        label: "Most significant peak in bin",
-        stroke: "#26A69A",
-        fill: "rgba(38, 166, 154, 0.15)",
-        paths: drawPoints,
-        values: (u, s, d) => {
-          const dd =  [u, s, d].includes(null) ? undefined : dataNoNulls[d];  // still undefined if data is empty
-          return dd === undefined ? ({
-            "SNP": "——————",
-            "Feature": "————",
-            "p": "—————",
-          }) : ({
-            "SNP": dd[snpProp],
-            "Feature": dd[geneProp] ?? dd[featureProp],
-            "p": dd[pValueProp].toPrecision(3),
-          });
-        },
-      },
-    ],
-    cursor: {
-      dataIdx(u, s) {
-        if (s !== 1) return;  // Wrong series
-        if (qt.current === null) return;  // No quadtree
-
-        const {left, top} = u.cursor;
-
-        const cx = left * pxr;
-        const cy = top * pxr;
-
-        const res = qt.current.find(cx - halfPointSize, cy - halfPointSize, POINT_SIZE * 1.6 * pxr);
-        const hi = res ? res[2] : undefined;
-        hoveredItem.current = hi;
-        return hi ?? null;
-      },
-      points: {
-        size: POINT_SIZE * pxr + STROKE_WIDTH,
-      },
-      bind: {
-        mouseup: (u, t, h) => {
-          console.log(u, t, h);
-          return e => {
-            if (e.button === 0 && hoveredItem.current) {
-              console.log(dataNoNulls[hoveredItem.current]);
-            }
-            h(e);
+            // D3-quadtree: index 0 is X, index 1 is Y, rest can be other stuff
+            newQt.add([
+              // cx - halfPointSize - STROKE_WIDTH / 2 - u.bbox.left,
+              cx - u.bbox.left - halfPointSize - STROKE_WIDTH,
+              // cy - halfPointSize - STROKE_WIDTH / 2 - u.bbox.top,
+              cy - u.bbox.top - halfPointSize - STROKE_WIDTH,
+              i,
+            ]);
           }
+        }
+
+        u.ctx.fill(p);
+        u.ctx.stroke(p);
+        u.ctx.restore();
+      });
+
+      qt.current = newQt;
+      return null;
+    }, [pxr]);
+
+    const hoveredItem = useRef(undefined);
+
+    // noinspection JSUnusedGlobalSymbols
+    const uPlotOptions = useMemo(() => ({
+      title: "chr1 RNA-seq: Most significant peaks by SNP position (25kb bins)",
+      mode: 2, // ?
+      width: 1110,
+      height: 300,
+      scales: {
+        x: {time: false},
+        y: {range: [1, maxY]},
+      },
+      axes: [
+        {
+          label: "Position",
+          // scale: "Mb",
+          values: (self, ticks) => ticks.map(v => `${v.toFixed(0)} Mb`),
+        },
+        {label: "-log10(p)"},
+      ],
+      series: [
+        {},  // weird uPlot hack to make scatter plots
+        {
+          label: "Most significant peak in bin",
+          stroke: "#26A69A",
+          fill: "rgba(38, 166, 154, 0.15)",
+          paths: drawPoints,
+          values: (u, s, d) => {
+            const dd =  [u, s, d].includes(null) ? undefined : dataNoNulls[d];  // still undefined if data is empty
+            return dd === undefined ? ({
+              "SNP": "——————",
+              "Feature": "————",
+              "p": "—————",
+            }) : ({
+              "SNP": dd[snpProp],
+              "Feature": dd[geneProp] ?? dd[featureProp],
+              "p": dd[pValueProp].toPrecision(3),
+            });
+          },
+        },
+      ],
+      cursor: {
+        dataIdx(u, s) {
+          if (s !== 1) return;  // Wrong series
+          if (qt.current === null) return;  // No quadtree
+
+          const {left, top} = u.cursor;
+
+          const cx = left * pxr;
+          const cy = top * pxr;
+
+          const res = qt.current.find(cx - halfPointSize, cy - halfPointSize, POINT_SIZE * 1.6 * pxr);
+          const hi = res ? res[2] : undefined;
+          hoveredItem.current = hi;
+          return hi ?? null;
+        },
+        points: {
+          size: POINT_SIZE * pxr + STROKE_WIDTH,
+        },
+        bind: {
+          mouseup: (u, t, h) => {
+            console.log(u, t, h);
+            return e => {
+              if (e.button === 0 && hoveredItem.current) {
+                onPointClick(dataNoNulls[hoveredItem.current]);
+              }
+              h(e);
+            }
+          },
         },
       },
-    },
-    hooks: {
-      drawClear: [u => {
-        qt.current = quadtree();
-        u.series.forEach((s, i) => {
-          if (i > 0) s._paths = null;  // Force a redraw to populate the quadtree
-        });
-      }],
-    },
-  }), [dataNoNulls, maxY, drawPoints, qt, pxr, halfPointSize]);
+      hooks: {
+        drawClear: [u => {
+          qt.current = quadtree();
+          u.series.forEach((s, i) => {
+            if (i > 0) s._paths = null;  // Force a redraw to populate the quadtree
+          });
+        }],
+      },
+    }), [dataNoNulls, maxY, drawPoints, qt, pxr, halfPointSize]);
 
-  // noinspection JSValidateTypes
-  return <div style={{boxSizing: "border-box", paddingTop: 16, textAlign: "center"}}>
-    <UplotReact options={uPlotOptions} data={finalData} />
-    <em style={{color: "#999"}}>Double-click to reset zoom.</em>
-  </div>;
-});
+    // noinspection JSValidateTypes
+    return <div style={{boxSizing: "border-box", paddingTop: 16, textAlign: "center"}}>
+      <UplotReact options={uPlotOptions} data={finalData} />
+      <em style={{color: "#999"}}>Double-click to reset zoom.</em>
+    </div>;
+  });
 
 export default ManhattanPlot;
--- client/src/components/ManhattanTest.js
+++ client/src/components/ManhattanTest.js
@@ -1,13 +1,19 @@
-import {Col, Row} from "reactstrap";
 import {useEffect, useState} from "react";
 import ManhattanPlot from "./ManhattanPlot";
+import {useNavigate} from "react-router-dom";
+
+const SNP_PROP = "snp_nat_id";
 
 const ManhattanTest = () => {
+  const navigate = useNavigate();
+
+  const assay = "RNA-seq";
+
   const [chr1RnaSeq, setChr1RnaSeq] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const res = await fetch("/api/overview/assays/RNA-seq/topBinned/1");
+      const res = await fetch(`/api/overview/assays/${assay}/topBinned/1`);
       const resJSON = await res.json();
       setChr1RnaSeq(resJSON.data);
     })();
@@ -18,9 +24,12 @@ const ManhattanTest = () => {
       data={chr1RnaSeq ?? []}
       positionProp="pos_bin"
       pValueProp="p_val"
-      snpProp="snp_nat_id"
+      snpProp={SNP_PROP}
       featureProp="feature_nat_id"
       geneProp="gene_name"
+      onPointClick={peak => (
+        navigate(`/explore/locus/rsID/${peak[SNP_PROP]}/${assay}`);
+      )}
     />
   </div>;
 };
