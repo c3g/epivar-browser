aaef75a7aae6a34c5a3943aa9487f93d412616b1
David Lougheed <david.lougheed@gmail.com>
2023-07-10 11:55:48 -0400

    refact: memoization in ProtectedPageContainer

--- client/src/components/pages/ProtectedPageContainer.js
+++ client/src/components/pages/ProtectedPageContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import {useSelector} from "react-redux";
 import {useOutletContext} from "react-router-dom";
 import {LOGIN_PATH} from "../../constants/app";
@@ -6,30 +6,37 @@ import Intro from "../Intro";
 import {Container, Spinner} from "reactstrap";
 import {setPostAuthPath} from "../../helpers/localStorage";
 
-const ProtectedPageContainer = ({children}) => {
-  const {setTermsModal} = useOutletContext();
-  const userData = useSelector(state => state.user);
-
-  if (!userData.isLoaded) return <Container>
+const LoadingContainer = React.memo(() => (
+  <Container>
     <div style={{textAlign: "center", marginTop: 48}}>
       <Spinner />
     </div>
-  </Container>;
+  </Container>
+));
 
-  return (!userData.data?.consentedToTerms) ? (
-    <Intro onTerms={() => setTermsModal(true)} onAccess={() => {
-      if (!userData.data) {
-        // Set our post-auth redirect and redirect to sign in, so we can capture some information about their identity.
-        setPostAuthPath(window.location.pathname);
-        window.location.href = `${LOGIN_PATH}?redirect=${window.location.pathname}`;
-      } else {
-        // Signed in but terms not accepted yet; show the modal.
-        setTermsModal(true);
-      }
-    }} signedIn={!!userData.data} />
+const ProtectedPageContainer = React.memo(({children}) => {
+  const {setTermsModal} = useOutletContext();
+  const {data: userData, isLoaded} = useSelector(state => state.user);
+
+  const onTerms = useCallback(() => setTermsModal(true), []);
+  const onAccess = useCallback(() => {
+    if (!userData) {
+      // Set our post-auth redirect and redirect to sign in, so we can capture some information about their identity.
+      setPostAuthPath(window.location.pathname);
+      window.location.href = `${LOGIN_PATH}?redirect=${window.location.pathname}`;
+    } else {
+      // Signed in but terms not accepted yet; show the modal.
+      setTermsModal(true);
+    }
+  }, [userData]);
+
+  if (!isLoaded) return <LoadingContainer />;
+
+  return (!userData?.consentedToTerms) ? (
+    <Intro onTerms={onTerms} onAccess={onAccess} signedIn={!!userData} />
   ) : <>
     {children}
   </>;
-};
+});
 
 export default ProtectedPageContainer;
