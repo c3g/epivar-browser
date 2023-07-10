d21812cea380cbe7fe1dac872a0c1f85c1801cd8
David Lougheed <david.lougheed@gmail.com>
2023-07-10 11:50:05 -0400

    chore(client): add verbiage for precomputed points mismatch with tracks

--- client/src/components/PeakBoxplot.js
+++ client/src/components/PeakBoxplot.js
@@ -54,12 +54,21 @@ function PeakBoxplot({ title, peak, /*values = defaultValues*/ }) {
         ))}
       </div>
       <div className="PeakBoxplot__disclaimer">
-        {!usePrecomputed && <p>
-          When not using precomputed values, box plots are generated from normalised signals (read count per base pair
-          per 10 million reads), without any batch correction, whereas <em>p</em>-values are calculated from the
-          age-regressed, batch-corrected signal values. The <em>p</em>-values thus may not precisely match the
-          distributions visible in the box plots.
-        </p>}
+        {
+          usePrecomputed
+            ? <p>
+              The box plots shown here use pre-computed, read-count normalized, batch corrected, and age-regressed
+              values. When a "Tracks" external link is clicked, the UCSC browser will be pre-loaded with bigWig data
+              merged on-the-fly using per-sample/condition signal data. These data ARE NOT batch-corrected nor
+              age-regressed, and thus may not match the distributions visible in the box plots.
+            </p>
+            : <p>
+              When not using precomputed values, box plots are generated from normalised signals (read count per base pair
+              per 10 million reads), without any batch correction, whereas <em>p</em>-values are calculated from the
+              age-regressed, batch-corrected signal values. The <em>p</em>-values thus may not precisely match the
+              distributions visible in the box plots.
+            </p>
+        }
       </div>
     </div>
   )
