835d3dfa854c671969f5827749e071bb8ed810c6
David Lougheed <david.lougheed@gmail.com>
2023-12-21 12:40:58 -0500

    chore(client): make FAQ dataset-generic

--- client/src/components/pages/FAQPage.js
+++ client/src/components/pages/FAQPage.js
@@ -8,41 +8,29 @@ const FAQPage = () => {
         <details open={true}>
           <summary>How are the QTL plots generated?</summary>
           <p>
-            To generate plots, we use the UCSC <em>bigWigSummary</em> tool to calculate average signal across a feature
-            for each sample upon request. These values are combined with genotype information extracted from a{" "}
+            To generate plots, we either use pre-computed, batch-corrected and normalized sample-wise signal matrices,
+            or use the UCSC <em>bigWigSummary</em> tool to calculate average signal across a feature for each sample
+            upon request. These values are combined with genotype information extracted from a{" "}
             <a href="https://gemini.readthedocs.io/en/latest/"
                target="_blank"
                rel="noreferrer">Gemini database</a> into a box plot on a server, which is
             then sent to the user’s browser if they have signed in and agreed to the terms of use. Plots are generated
-            as needed, rather than in advance, and are derived directly from the count matrix or track and genotype
-            data.
+            as needed, rather than in advance, and are derived directly from the matrix or track and genotype data.
           </p>
         </details>
         <details open={true}>
           <summary>How were the genomic tracks grouped by genotype generated?</summary>
           <p>
-            Genome browser tracks were created with the HOMER <em>makeUCSCfile</em> command and{" "}
-            <em>bedGraphToBigWig</em> utility from UCSC. Tracks were normalized so that each value represents the read
-            count per base pair per 10 million reads.  We generate{" "}
+            We generate{" "}
             <a href="https://genome.ucsc.edu/"
                target="_blank"
                rel="noreferrer">UCSC Genome Browser</a> track hubs upon
-            request to visualize averaged track segments for a given genomic feature. These tracks are created on the
-            fly by averaging bigWig regions of samples sharing an experimental treatment and genotype, using our
-            command-line tool:
+            request to visualize averaged normalized track segments for a given genomic feature. These averaged tracks
+            are created on the fly by averaging bigWig regions of samples sharing an experimental treatment and
+            genotype, using our command-line tool:
             <a href="https://github.com/c3g/bw-merge-window"
                target="_blank"
                rel="noreferrer"><code>bw-merge-window</code></a>.
-          </p>
-        </details>
-        <details open={true}>
-          <summary>How were the global tracks per ancestry group generated?</summary>
-          <p>
-            The dark shaded area denotes the distribution of the average RPM values and the light shaded area denotes
-            the standard deviation. Signals of various epigenetic marks are shown in blue color for non-infected samples
-            and red color for infected samples. For RNA-seq, forward and reverse transcripts are shown in blue and green
-            color separately for non-infected samples; while forward and reverse transcripts are shown in red and brown
-            color separately for infected samples.
           </p>
         </details>
       </Col>
