61be70c52cc50c62c612a241510d28a57919c4ed
David Lougheed <david.lougheed@gmail.com>
2024-01-04 12:15:03 -0500

    chore(portal): update FAQ: replace GEMINI with VCF and add igv.js mention

--- client/src/components/pages/FAQPage.js
+++ client/src/components/pages/FAQPage.js
@@ -10,12 +10,10 @@ const FAQPage = () => {
           <p>
             To generate plots, we either use pre-computed, batch-corrected and normalized sample-wise signal matrices,
             or use the UCSC <em>bigWigSummary</em> tool to calculate average signal across a feature for each sample
-            upon request. These values are combined with genotype information extracted from a{" "}
-            <a href="https://gemini.readthedocs.io/en/latest/"
-               target="_blank"
-               rel="noreferrer">Gemini database</a> into a box plot on a server, which is
-            then sent to the user’s browser if they have signed in and agreed to the terms of use. Plots are generated
-            as needed, rather than in advance, and are derived directly from the matrix or track and genotype data.
+            upon request. These values are combined with genotype information, extracted from a VCF file, to create a
+            box plot on a server, which is then sent to the user’s browser if they have agreed to the terms of use.
+            Plots are generated as needed, rather than in advance, and are derived directly from the matrix or track and
+            genotype data.
           </p>
         </details>
         <details open={true}>
@@ -24,10 +22,11 @@ const FAQPage = () => {
             We generate{" "}
             <a href="https://genome.ucsc.edu/"
                target="_blank"
-               rel="noreferrer">UCSC Genome Browser</a> track hubs upon
-            request to visualize averaged normalized track segments for a given genomic feature. These averaged tracks
-            are created on the fly by averaging bigWig regions of samples sharing an experimental treatment and
-            genotype, using our command-line tool:{" "}
+               rel="noreferrer">UCSC Genome Browser</a> track hubs or{" "}
+            <a href="https://github.com/igvteam/igv.js/wiki" target="_blank" rel="noreferrer">IGV.js</a>{" "}
+            browser instances upon request to visualize averaged normalized track segments for a given genomic feature.
+            These averaged tracks are created on the fly by averaging bigWig regions of samples sharing an experimental
+            treatment and genotype, using our command-line tool:{" "}
             <a href="https://github.com/c3g/bw-merge-window"
                target="_blank"
                rel="noreferrer"><code>bw-merge-window</code></a>.
