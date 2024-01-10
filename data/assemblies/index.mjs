fd81c09ac33109c6d0ad51e2139a83c56c0fa0bb
David Lougheed <david.lougheed@gmail.com>
2024-01-10 14:17:28 -0500

    chore: set up paths for gene files by assembly

--- data/assemblies/index.mjs
+++ data/assemblies/index.mjs
@@ -1,7 +1,14 @@
+import path from "node:path";
+
 import hg19Sizes from "./hg19/chromosomeSizes.mjs";
 import hg38Sizes from "./hg38/chromosomeSizes.mjs";
 
 export const chromosomeSizesByAssemblyID = {
   "hg19": hg19Sizes,
   "hg38": hg38Sizes,
+};
+
+export const genePathsByAssemblyID = {
+  "hg19": path.join(__dirname, "hg19", "genes.txt"),
+  "hg38": path.join(__dirname, "hg38", "genes.txt"),
 };
