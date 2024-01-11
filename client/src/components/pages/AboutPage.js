f6b5b7291359e2ffb70ba950dbb52bec7a84f018
David Lougheed <david.lougheed@gmail.com>
2024-01-11 10:13:17 -0500

    chore(client): add new about page for portal

--- client/src/components/pages/AboutPage.js
+++ client/src/components/pages/AboutPage.js
@@ -15,50 +15,46 @@ const AboutPage = () => {
   return <Container className="Page">
     <Row>
       <Col md="12" lg={{size: 10, offset: 1}}>
-        <h2>About</h2>
+        <h2>About the EpiVar Browser</h2>
+        <h3>Approach</h3>
         <p>
-          Humans display remarkable immune response variation when exposed to identical immune challenges.
-          For this project, we carried out in-depth genetic, epigenetic, and transcriptional profiles on primary
-          macrophages derived from a panel of European and African-ancestry individuals, before and after
-          infection with influenza A virus (IAV) (see below).
-        </p>
-        <figure>
-          <img width={600} height={387} src="/figure1A.png" alt="Figure showing methodology" />
-        </figure>
-        <p id="papers">
-          The results of this project are summarized in 3 papers:
-        </p>
-        <ol>
-          <li>
-            <a href="https://www.biorxiv.org/content/10.1101/2022.05.10.491413v1" target="_blank" rel="nofollow">
-              Aracena <em>et al</em>. Epigenetic variation impacts ancestry-associated differences in the
-              transcriptional response to influenza infection.
-            </a>
-          </li>
-          <li>
-            <a href="https://doi.org/10.1016/j.xgen.2023.100292"  target="_blank" rel="nofollow">
-              Chen <em>et al</em>. Transposable elements are associated with the variable response to
-              influenza infection.
-            </a>
-          </li>
-          <li>
-            <a href="https://doi.org/10.1016/j.xgen.2023.100294" target="_blank" rel="nofollow">
-              Groza <em>et al</em>. Genome graphs detect human polymorphisms in active epigenomic state during
-              influenza infection.
-            </a>
-          </li>
-        </ol>
-        <p>
-          All the datasets that have been generated as part of this project are summarized and{" "}
-          <Link to="/datasets">made available here</Link>. Access to some of the raw datasets require a formal data
-          access request but all resources are available freely. Please cite the appropriate paper(s) if you use
-          the data.
+          The EpiVar Browser is a federated portal that enables exploration of controlled-access to genetic and
+          epigenetic datasets, allowing researchers to explore the interaction between genomic variants and epigenetic
+          features, such as histone tail modifications, chromatin accessibility, and the transcriptome. Because the
+          information about individual genotypes is not accessible, identifiable data is not released. We believe our
+          approach can accelerate analyses that would otherwise require a lengthy multi-step approval process and
+          provides a generalisable strategy to facilitate responsible access to sensitive epigenomics data.
         </p>
         <p>
-          We have also built a <Link to="/dataset/overview" style={{fontWeight: "bold"}}>browser tool</Link> that allows
-          navigation of these rich datasets by condition, ancestry and, importantly, haplotype. This allows an in-depth
-          exploration of the quantitative trait loci and other key results reported in the studies above. Using this
-          tool requires a simple terms-of-use agreement to ensure data privacy conditions are respected.
+          The browser is described in the following preprint:
+        </p>
+        <p>
+          <a href="" target="_blank" rel="nofollow">
+            EpiVar Browser: advanced exploration of epigenomics data under controlled access
+          </a><br />
+          David R Lougheed, Hanshi Liu, Katherine A Aracena, Romain Grégoire, Alain Pacis, Tomi Pastinen,
+          Luis B Barreiro, Yann Joly, David Bujold, Guillaume Bourque.<br />
+          bioRxiv 2023.08.03.551309; doi:{" "}
+          <a href="https://doi.org/10.1101/2023.08.03.551309" target="_blank" rel="nofollow">
+            10.1101/2023.08.03.551309</a>
+        </p>
+        <h3>EpiVar Architecture</h3>
+        <p>
+          We employ a federated approach to build a multi-dataset portal where data processing can, as much as possible,
+          occur alongside the data location. Our front end contains a list of nodes, each of which hosts a single
+          dataset:
+        </p>
+        <img
+          src="/epivar_arch.png"
+          alt="A workflow diagram of the EpiVar Browser, showing how the browser connects to multiple nodes."
+          width="3829"
+          height="2591"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <h3>Contact</h3>
+        <p>
+          If you have any questions about the browser or would like to report a bug or other issue, contact us at
+          <a href="mailto:epivar@computationalgenomics.ca">epivar@computationalgenomics.ca</a>.
         </p>
       </Col>
     </Row>
