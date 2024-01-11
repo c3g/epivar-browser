3bec54c26589715cd476c3a1866cf756d5d030b5
David Lougheed <david.lougheed@gmail.com>
2024-01-11 15:27:30 -0500

    chore: gene renaming

--- models/genes.mjs
+++ models/genes.mjs
@@ -8,14 +8,21 @@ const GENE_RENAMES = {
   FAM214B: "ATOSB",
   BZRAP1: "TSPOAP1",
   C19orf60: "REX1BD",
+  KIAA0100: "BLTP2",
+  CRAMP1L: "CRAMP1",
+  TCEB3: "ELOA",
 };
 
-export const normalizeGeneName = name => {
-  const nameNorm = name.replace(/[^a-zA-Z\d]/g, '-');
-  if (nameNorm in GENE_RENAMES) return GENE_RENAMES[nameNorm];
-  return nameNorm;
-}
+export const renameGeneIfNeeded = (name) => {
+  if (name in GENE_RENAMES) return GENE_RENAMES[name];
+  return name;
+};
+
+export const normalizeGeneName = (name) => {
+  return renameGeneIfNeeded(name.replace(/[^a-zA-Z\d]/g, '-'));
+};
 
 export default {
+  renameGeneIfNeeded,
   normalizeGeneName,
 };
