aff43ba3d122215983f6be3602dbada72a806d96
David Lougheed <david.lougheed@gmail.com>
2024-01-16 12:27:18 -0500

    fix: allow linking headings in dataset about.md

--- routes/dataset.mjs
+++ routes/dataset.mjs
@@ -1,11 +1,14 @@
 import fs from "node:fs/promises";
 import express from "express";
 import {marked} from "marked";
+import {gfmHeadingId} from "marked-gfm-heading-id";
 
 import config from "../config.js";
 import envConfig from "../envConfig.js";
 import {chromosomeSizesByAssemblyID} from "../data/assemblies/index.mjs";
 import {dataHandler} from "../helpers/handlers.mjs";
+
+marked.use(gfmHeadingId());
 
 const aboutContent = marked.parse(await fs.readFile(envConfig.ABOUT_MD_PATH, {encoding: "utf-8"}));
 
