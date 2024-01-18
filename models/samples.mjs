bbc8f9ea6ff432467432de1263de90bcefc397a9
David Lougheed <david.lougheed@gmail.com>
2024-01-18 14:19:56 -0500

    chore: log vcf queries

--- models/samples.mjs
+++ models/samples.mjs
@@ -37,6 +37,7 @@ export async function vcfQuery(contig, start, end) {
   const lines = [];
   // tabix JS takes in 0-based half-open coordinates, which we convert from queryMap taking 1-based closed coordinates
   await VCF_TABIX_FILE.getLines(contig, start - 1, end, line => lines.push(vcfParser.parseLine(line)));
+  console.info(`queried vcf: ${contig}:${start}-${end}; got ${lines.length} lines`);
   return lines;
 }
 
